6e44c1f8f628e75048118ad9dd99be98b804ecde
Marius DAMIEN <marius.damien@gmail.com>
2021-08-10 16:51:15 +0200

    [PYTHON] - pattern -> UTILOC

--- extractLoc.py
+++ extractLoc.py
@@ -3,7 +3,7 @@ import os
 import fnmatch
 
 
-pattern = re.compile(">(?s)(.*)</Text>")
+pattern = re.compile("Util.loc\(\"(.*)\"\)", flags=re.DOTALL)
 exclude = set(['node_modules'])
 
 for root, dirs, files in os.walk("./"):
@@ -11,7 +11,7 @@ for root, dirs, files in os.walk("./"):
 	for file in files:
 		if(file.endswith(".js")):
                     path = str(os.path.join(root,file))
-                    for i, line in enumerate(open(path)):
-                       # for match in re.finditer(pattern, line):
-                        for match in re.finditer(pattern, line):
-                            print "Line %s: str ----> '%s' fichier ----> '%s'" % (i+1, match.group(1), path)
+                    fd = open(path)
+                    fd = fd.read()
+                    for match in re.finditer(pattern, fd):
+                        print "str ----> '%s' fichier ----> '%s'" % (match.group(1), path)
